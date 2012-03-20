35ce44caeaae8188186dd249781a53776574d35a
Alain <snowgoon88@gmail.com>
2012-03-20 22:33:38 +0100

    cleaning up

--- scigl/trackball.o.d
+++
@@ -1,6 +0,0 @@
-scigl/trackball.o scigl/trackball.o.d: scigl/trackball.cc /usr/include/math.h \
-  /usr/include/architecture/i386/math.h /usr/include/sys/cdefs.h \
-  scigl/trackball.h
-scigl/trackball.cc /usr/include/math.h:
-/usr/include/architecture/i386/math.h /usr/include/sys/cdefs.h:
-scigl/trackball.h:
